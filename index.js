fd26947a9ab2a7c63c00b416d38860d6c6c6cb9b
Mmackz <matthewmackenz@gmail.com>
2022-03-18 21:50:54 -0700

    ♻️ refactor and add keyboard support

--- index.js
+++ index.js
@@ -15,6 +15,50 @@ let lastPressed = "";
 subDisplayEl.innerText = firstNum;
 mainDisplayEl.innerText = "0";
 
+function adjustByLength() {
+   // changes font-size dynamically based on length of string
+   if (mainDisplayEl.innerText.length > 10) {
+      if (!mainDisplayEl.classList.contains("small")) {
+         mainDisplayEl.classList.add("small");
+      }
+   } else {
+      mainDisplayEl.classList.remove("small");
+   }
+
+   if (subDisplayEl.innerText.length > 20) {
+      if (!subDisplayEl.classList.contains("st-small")) {
+         subDisplayEl.classList.add("st-small");
+      }
+   } else {
+      subDisplayEl.classList.remove("st-small");
+   }
+
+   if (subDisplayEl.innerText.length > 32) {
+      alert("Input is too long!!");
+      firstNum = "";
+      operator = "";
+      lastNum = "";
+      mainDisplayEl.innerText = "0";
+   }
+}
+
+function formatDisplay(value) {
+   // remove trailing decimal points
+   if (operator && firstNum.endsWith(".")) {
+      firstNum = firstNum.slice(0, -1);
+   }
+
+   subDisplayEl.innerText = `${firstNum} ${operator
+      .replace("*", "x")
+      .replace("/", "÷")} ${lastNum}`;
+   lastPressed = value;
+
+   // remove trailing decimals on enter press
+   if (lastPressed == "equals" && subDisplayEl.innerText.endsWith(".")) {
+      subDisplayEl.innerText = subDisplayEl.innerText.slice(0, -1);
+   }
+}
+
 function updateDisplay() {
    firstNum = calculate(firstNum, lastNum, operator).toString();
    lastNum = "";
@@ -22,141 +66,108 @@ function updateDisplay() {
 }
 
 function onButtonPress(value) {
-         // if power button is pressed
-         if (value === "power") {
-            firstNum = "";
-            operator = "";
-            lastNum = "";
-            mainDisplayEl.innerText = "0";
-         }
-   
-         // if delete button is pressed
-         if (value === "delete") {
-            if (lastNum) {
-               lastNum = lastNum.slice(0, -1);
-            } else if (operator) {
-               operator = "";
-            } else {
-               firstNum = firstNum.slice(0, -1);
-            }
-         }
-   
-         // if input is a number
-         if (Number.isInteger(+value)) {
-            if (lastPressed == "equals") {
-               firstNum = value;
-               operator = "";
-               lastNum = "";
-            } else if (lastNum && lastNum === "0") {
-               lastNum = value;
-            } else if (!operator && firstNum === "0") {
-               firstNum = value;
-            } else if (operator) {
-               lastNum += value;
-            } else {
-               firstNum += value;
-            }
-         }
-   
-         // if input is a decimal
-         if (value === "decimal") {
-            if (lastPressed == "equals") {
-               firstNum = "0.";
-               operator = "";
-               lastNum = "";
-            } else if (operator && !lastNum.includes(".")) {
-               if (!lastNum) {
-                  lastNum = "0.";
-               } else {
-                  lastNum += ".";
-               }
-            } else if (!operator && !firstNum.includes(".")) {
-               if (!firstNum) {
-                  firstNum = "0.";
-               } else {
-                  firstNum += ".";
-               }
-            }
-         }
-   
-         // operators
-         if (value === "divide") {
-            if (lastNum) {
-               updateDisplay();
-            }
-            operator = "/";
-         }
-   
-         if (value === "multiply") {
-            if (lastNum) {
-               updateDisplay();
-            }
-            operator = "*";
-         }
-   
-         if (value === "plus") {
-            if (lastNum) {
-               updateDisplay();
-            }
-            operator = "+";
-         }
-   
-         if (value === "minus") {
-            if (lastNum) {
-               updateDisplay();
-            }
-            operator = "-";
-         }
-   
-         if (value === "equals") {
-            if (lastPressed == "equals") {
-               firstNum = mainDisplayEl.innerText;
-            }
-            if (firstNum && lastNum && operator) {
-               mainDisplayEl.innerText = calculate(firstNum, lastNum, operator);
-            } else {
-               mainDisplayEl.innerText = firstNum || "0";
-            }
-         }
-   
-         if (mainDisplayEl.innerText.length > 10) {
-            if (!mainDisplayEl.classList.contains("small")) {
-               mainDisplayEl.classList.add("small");
-            }
-         } else {
-            mainDisplayEl.classList.remove("small");
-         }
-   
-         if (subDisplayEl.innerText.length > 20) {
-            if (!subDisplayEl.classList.contains("st-small")) {
-               subDisplayEl.classList.add("st-small");
-            }
-         } else {
-            subDisplayEl.classList.remove("st-small");
-         }
-   
-         if (subDisplayEl.innerText.length > 32) {
-            alert("Input is too long!!");
-            firstNum = "";
-            operator = "";
-            lastNum = "";
-            mainDisplayEl.innerText = "0";
-         }
+   // if power button is pressed
+   if (value === "power") {
+      firstNum = "";
+      operator = "";
+      lastNum = "";
+      mainDisplayEl.innerText = "0";
+   }
 
-         // remove trailing decimal points
-         if (operator && firstNum.endsWith(".")) {
-            firstNum = firstNum.slice(0, -1);
-         }
-   
-         subDisplayEl.innerText = `${firstNum} ${operator
-            .replace("*", "x")
-            .replace("/", "÷")} ${lastNum}`;
-         lastPressed = value;
+   // if delete button is pressed
+   if (value === "delete") {
+      if (lastNum) {
+         lastNum = lastNum.slice(0, -1);
+      } else if (operator) {
+         operator = "";
+      } else {
+         firstNum = firstNum.slice(0, -1);
+      }
+   }
 
-         // remove trailing decimals on enter press
-         if (lastPressed == "equals" && subDisplayEl.innerText.endsWith(".")) {
-            subDisplayEl.innerText = subDisplayEl.innerText.slice(0, -1);
+   // if input is a number
+   if (Number.isInteger(+value)) {
+      if (lastPressed == "equals") {
+         firstNum = value;
+         operator = "";
+         lastNum = "";
+      } else if (lastNum && lastNum === "0") {
+         lastNum = value;
+      } else if (!operator && firstNum === "0") {
+         firstNum = value;
+      } else if (operator) {
+         lastNum += value;
+      } else {
+         firstNum += value;
+      }
+   }
+
+   // if input is a decimal
+   if (value === "decimal") {
+      if (lastPressed == "equals") {
+         firstNum = "0.";
+         operator = "";
+         lastNum = "";
+      } else if (operator && !lastNum.includes(".")) {
+         if (!lastNum) {
+            lastNum = "0.";
+         } else {
+            lastNum += ".";
          }
+      } else if (!operator && !firstNum.includes(".")) {
+         if (!firstNum) {
+            firstNum = "0.";
+         } else {
+            firstNum += ".";
+         }
+      }
+   }
+
+   // operators
+   if (value === "divide") {
+      if (lastNum) {
+         updateDisplay();
+      }
+      operator = "/";
+   }
+
+   if (value === "multiply") {
+      if (lastNum) {
+         updateDisplay();
+      }
+      operator = "*";
+   }
+
+   if (value === "plus") {
+      if (lastNum) {
+         updateDisplay();
+      }
+      operator = "+";
+   }
+
+   if (value === "minus") {
+      if (lastNum) {
+         updateDisplay();
+      }
+      operator = "-";
+   }
+
+   if (value === "equals") {
+      if (lastPressed == "equals") {
+         firstNum = mainDisplayEl.innerText;
+      }
+      if (firstNum && lastNum && operator) {
+         mainDisplayEl.innerText = calculate(firstNum, lastNum, operator);
+      } else {
+         mainDisplayEl.innerText = firstNum || "0";
+      }
+   }
+
+   // adjusts font-size of string based on length
+   adjustByLength();
+
+   // formats and updates display element
+   formatDisplay(value);
 }
 
 allButtons.forEach((button) => {
@@ -167,8 +178,8 @@ allButtons.forEach((button) => {
    });
 });
 
+// keypress listeners
 window.addEventListener("keypress", (event) => {
-   console.log(event.key)
    if (Number.isInteger(+event.key)) {
       onButtonPress(event.key);
    } else {
@@ -179,6 +190,28 @@ window.addEventListener("keypress", (event) => {
          case "Enter":
             onButtonPress("equals");
             break;
+         case "=":
+            onButtonPress("equals");
+            break;
+         case "/":
+            onButtonPress("divide");
+            break;
+         case "*":
+            onButtonPress("multiply");
+            break;
+         case "+":
+            onButtonPress("plus");
+            break;
+         case "-":
+            onButtonPress("minus");
+            break;
+         case "Delete":
+            onButtonPress("delete");
+            break;
       }
    }
-})
+});
+
+// give attribution to design inspiration inside console
+console.log("style and layout inspired by Víctor Arenas https://dribbble.com/shots/2858221-Daily-UI-004-Calculator-UI-Design")
+console.log("gradients from https://uigradients.com")
